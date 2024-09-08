2082ae5a66eee9627dd29a3c981be363cfe85a93
Stawa <stawamusic@gmail.com>
2024-09-08 17:52:36 +0800

    Update route.ts

--- app/api/instagram/route.ts
+++ app/api/instagram/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { fetchUser } from "./fetchUser";
 
+export const runtime = "edge";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const username = searchParams.get("username");
